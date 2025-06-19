200b916cf35a778583710b1441c1403a5d45dc42
Samir Maciel <samirmaciel.dev@gmail.com>
2025-06-18 23:02:05 -0300

    first commit

---
+++ app/src/main/java/com/sm/kotlincoroutinesplayground/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.sm.kotlincoroutinesplayground
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sm.kotlincoroutinesplayground.ui.theme.KotlinCoroutinesPlaygroundTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            KotlinCoroutinesPlaygroundTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    KotlinCoroutinesPlaygroundTheme {
+        Greeting("Android")
+    }
+}
